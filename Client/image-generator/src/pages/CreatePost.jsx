46bac2286544c95fa48f3aba07882660fd4629c9
Sanju Devadas <sanjudevadas11@gmail.com>
2024-07-29 21:07:09 +0530

    editing createpost.jsx

--- Client/image-generator/src/pages/CreatePost.jsx
+++ Client/image-generator/src/pages/CreatePost.jsx
@@ -1,9 +1,12 @@
 import React , {useState} from 'react'
 import { useNavigate } from 'react-router-dom'
-import {preview} from '../assets/preview.png'
+import preview from '../assets/preview.png'
 import {getRandomPrompt} from '../utils'
 import { FormField,Loader } from '../components'
+
 const CreatePost = () => {
+
+  const navigate= useNavigate();
   return (
     <div>CreatePost</div>
   )
